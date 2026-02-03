bd7cf34fc1b2ca636175cc81a6c3b1ad701feeba
officialmofabs <90117777+officialmofabs@users.noreply.github.com>
2026-02-03 20:02:14 +0000

    initial-config

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,5 +8,5 @@ alias ap="ansible-playbook"
 alias ls="eza --icons --group-directories-first"
 alias ll="eza --icons --group-directories-first -l"
 alias bp="boilerplate"
-alias prx="proxmox-manager"
+alias mgm="mosgarage-manager"
 
